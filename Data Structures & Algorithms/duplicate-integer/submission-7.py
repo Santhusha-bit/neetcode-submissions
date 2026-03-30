33fd6893fa273feb5bc42d72638b16088fd48fc1
Santhusha Janana Mudannayaka <74499826+Santhusha-bit@users.noreply.github.com>
2026-03-30 03:43:52 -0700

    Bulk sync: 23 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # dict = {}
+        # for i in nums:
+        #     if i in dict:
+        #         return True
+        #     else:
+        #         dict[i]=1
+        # return False
+
+        return len(set(nums)) != len(nums) 
+        
---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+
+        left, right = 0, len(cleaned) - 1
+
+        while left < right:
+            if cleaned[left] != cleaned[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
+        
